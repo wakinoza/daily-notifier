60e67bf9b852899621082bdde39180d0bc642f7b
wakinoza <wakinoza@gmail.com>
2026-07-20 15:54:40 +0900

    気象庁JSONデータを取得できることを確認

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
+import requests
+
 def main():
-    print("Hello, daily-notifier!")
+    URL = "https://www.jma.go.jp/bosai/forecast/data/forecast/230000.json"
+    response = requests.get(URL)
+    response.raise_for_status()
+
+    weather_data = response.json()
+
+    print(type(weather_data))
+    print(len(weather_data))
+    print(weather_data[0]["reportDatetime"])
+    tomorrow_weather = weather_data[0]["timeSeries"][0]["areas"][0]["weathers"][1]
+    print(tomorrow_weather)
 
 if __name__ == "__main__":
     main()
