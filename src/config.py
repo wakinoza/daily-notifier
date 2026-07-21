c447fc183e720eb03c26d03bd90c1cc75127993f
wakinoza <wakinoza@gmail.com>
2026-07-21 11:30:04 +0900

    _init_.pyとconfig.pyの定義。
    config.pyには定数を格納する

---
+++ src/config.py
@@ -0,0 +1,10 @@
+
+# 気象庁の予報JSON URL
+JMA_FORECAST_URL = "http://www.jma.go.jp/bosai/forecast/data/forecast/230000.json"
+
+
+# 天気予報を取得する地域
+JMA_FORECAST_AREA_NAME = "東部"
+
+# 気温を取得する地点
+JMA_TEMPERATURE_AREA_NAME = "名古屋"
