d3dedb76aa27ebaeeb2c5ca9033ebcb8495437d3
wakinoza <wakinoza@gmail.com>
2026-09-16 11:25:17 +0900

    koyomi.pyの処理を追加

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from logging import getLogger, basicConfig, INFO
+from datetime import date, timedelta
 
 from src.config import Settings
-from src.providers import weather_json
+from src.providers import weather_json, koyomi
 from src.formatters import weather_formatter, message
 from src.mailer import send_mail
 
@@ -20,10 +21,14 @@ def main() -> None:
     settings = Settings.from_environment()
 
     forecast_json = weather_json.fetch_forecast_json()
-
     weather_info = weather_json.extract_weather_info(forecast_json)
 
     weather_section = weather_formatter.format_weather_section(weather_info)
+
+    target_date = date.today() + timedelta(days=1)
+    koyomi_json_data = koyomi.fetch_koyomi(target_date.year)
+    koyomi_data = koyomi.parse_koyomi_json(koyomi_json_data)
+    events = koyomi.find_events(koyomi_data, target_date)
 
     send_mail("daily-notifier", message.create_message(weather_section), settings)
 
